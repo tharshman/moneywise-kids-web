e9e8b4e4c97f035432958a6f2f3f7ebc2479fa08
Vincent Bortone <vbortone@gmail.com>
2023-12-03 21:44:43 -0500

    feat: add new logo

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,45 +1,23 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import MoneyWiseLogo from '@/app/ui/moneywise-logo';
 
 export default function Header() {
   return (
-    <header className="navbar bg-secondary mb-4">
-      <div className="navbar-start">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-
-        <Link className="btn btn-ghost text-xl" key="Home" href="/">MoneyWise Kids</Link>
-        <ul className="menu menu-horizontal px-1">
+    <header className="navbar bg-secondary my-4 py-4">
+      <div className="navbar-start">        
+        <Link className="btn btn-ghost text-xl" key="Home" href="/">
+          <MoneyWiseLogo />
+        </Link>
+        <ul className="menu menu-horizontal px-1 text-mw-brown font-extrabold">
           <li>
             <Link href="/dashboard" key="Dashboard">Dashboard</Link>
           </li>
           <li>
-            <details>
-              <summary>Course Catalog</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Banking</a>
-                </li>
-                <li>
-                  <a>Stock Market</a>
-                </li>
-              </ul>
-            </details>
-          </li>
+            <Link href="/catalog" key="Catalog">Courses</Link>
+          </li>          
           <li>
-            <a>Partners</a>
+            <Link href="/partners" key="Partners">Partners</Link>            
           </li>
         </ul>
       </div>
